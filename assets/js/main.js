b76772e74a9d89cee65aa23fa6acd36719b5ce3c
TrullyItsup <itsup.pci@gmail.com>
2025-08-25 18:12:45 +0700

    chore: update headline & links

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 (function(){
-  const links = { order: "", lokasi: "", kemitraan: "" };
+  const links = { order: "https://esborder.qs.esb.co.id/outlet?brand=175", lokasi: "https://www.google.com/maps/search/malacca+toast/@-6.1261586,106.7751502,13z?entry=ttu", kemitraan: "https://friendchised.typeform.com/to/mXuY2W9s?utm_source=instagram+organic&utm_medium=in+message&utm_campaign=external+presence&typeform-source=bit.ly" };
   const setIf = (id, url) => { if (url) document.getElementById(id).setAttribute("href", url); };
   setIf("order-link", links.order);
   setIf("lokasi-link", links.lokasi);
